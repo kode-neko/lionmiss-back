f4ddc07d025d004fd2be65a039fdf1b8d370fae1
Kodeneko <ladysun.freedom@gmail.com>
2023-04-05 13:45:36 +0200

    [dev] delete eslint ignore and some folders

--- dist/model/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.builderCart = void 0;
-var utils_1 = require("./utils");
-Object.defineProperty(exports, "builderCart", { enumerable: true, get: function () { return utils_1.builderCart; } });
-//# sourceMappingURL=index.js.map
