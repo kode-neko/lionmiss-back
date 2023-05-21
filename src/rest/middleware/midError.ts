cbd45b3312e24b1cf125db954ef02a7200b18a3d
Kodeneko <ladysun.freedom@gmail.com>
2023-05-21 23:56:34 +0200

    [main] Fix error

--- src/rest/middleware/midError.ts
+++ src/rest/middleware/midError.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 function logHandler(req: Request, res: Response, next: NextFunction) {
   console.log(`Received a ${req.method} request for ${req.url}`);
-  next(req);
+  next();
 }
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
